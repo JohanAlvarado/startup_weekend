2cae63b1ad832b0ae2f3051952ff64b81fed3f64
Johan Alvarado <johan@Johans-MacBook-Pro.local>
2014-05-24 17:32:46 -0500

    change api keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['515636818562518'], ENV['fc5984f9a2b77d5c087e2fe48a1e7894']
+  provider :facebook, '515636818562518', 'fc5984f9a2b77d5c087e2fe48a1e7894'
 end
