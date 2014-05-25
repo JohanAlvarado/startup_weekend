5ff2df816bc1e797a0352c4fb4b41aab0737191c
Johan <johan.alvaradohdz@gmail.com>
2014-05-24 23:25:45 -0500

    change colors

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
   $.fn.fullpage({
-    menu: '.navbar',
     verticalCentered: true,
+    slidesColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#1bbc9b'],
     resize : false,
     anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
     navigation: true,
     navigationPosition: 'right',
     navigationTooltips:['firstPageTooltip', 'secondPageTooltip', 'thirdPageTooltip', 'fourthPageTooltip'],
-    css3: true
+    css3: true,
+    menu: '#menu'
   });
 });
